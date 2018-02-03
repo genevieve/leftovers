e3cfd07760fda823f83e9051d122598df6f94970
Genevieve LEsperance <glesperance@pivotal.io>
2018-02-03 12:35:45 -0800

    Add deleting for aws rds db instances.

--- aws/rds/fakes/db_instances_client.go
+++ aws/rds/fakes/db_instances_client.go
@@ -3,6 +3,13 @@ package fakes
 import awsrds "github.com/aws/aws-sdk-go/service/rds"
 
 type DBInstancesClient struct {
+	DescribeDBInstancesCall struct {
+		CallCount int
+		Returns   struct {
+			Output *awsrds.DescribeDBInstancesOutput
+			Error  error
+		}
+	}
 	DeleteDBInstanceCall struct {
 		CallCount int
 		Receives  struct {
@@ -20,4 +27,10 @@ func (d *DBInstancesClient) DeleteDBInstance(input *awsrds.DeleteDBInstanceInput
 	d.DeleteDBInstanceCall.Receives.Input = input
 
 	return d.DeleteDBInstanceCall.Returns.Output, d.DeleteDBInstanceCall.Returns.Error
+}
+
+func (d *DBInstancesClient) DescribeDBInstances(input *awsrds.DescribeDBInstancesInput) (*awsrds.DescribeDBInstancesOutput, error) {
+	d.DescribeDBInstancesCall.CallCount++
+
+	return d.DescribeDBInstancesCall.Returns.Output, d.DescribeDBInstancesCall.Returns.Error
 }
