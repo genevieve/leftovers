677d3bdd15b8527c76fa66708b0226e51ac03ee9
Connor Braa <connor.braa@gmail.com>
2018-02-09 13:45:41 -0800

    Parallelize gcp delete requests by resource.
    Signed-off-by: Genevieve LEsperance <glesperance@pivotal.io>

--- gcp/leftovers.go
+++ gcp/leftovers.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"sync"
 
 	"github.com/genevieve/leftovers/gcp/common"
 	"github.com/genevieve/leftovers/gcp/compute"
@@ -25,7 +26,7 @@ type Leftovers struct {
 }
 
 func (l Leftovers) Delete(filter string) error {
-	var deletables []common.Deletable
+	deletables := [][]common.Deletable{}
 
 	for _, r := range l.resources {
 		list, err := r.List(filter)
@@ -33,19 +34,34 @@ func (l Leftovers) Delete(filter string) error {
 			l.logger.Println(err.Error())
 		}
 
-		deletables = append(deletables, list...)
+		deletables = append(deletables, list)
 	}
 
-	for _, d := range deletables {
-		l.logger.Println(fmt.Sprintf("Deleting %s.", d.Name()))
+	var wg sync.WaitGroup
 
-		err := d.Delete()
+	for _, list := range deletables {
 
-		if err != nil {
-			l.logger.Println(err.Error())
-		} else {
-			l.logger.Println(fmt.Sprintf("SUCCESS deleting %s!", d.Name()))
+		for _, d := range list {
+
+			wg.Add(1)
+
+			l.logger.Println(fmt.Sprintf("Deleting %s.", d.Name()))
+
+			go func(d common.Deletable) {
+				defer wg.Done()
+
+				err := d.Delete()
+
+				if err != nil {
+					l.logger.Println(err.Error())
+				} else {
+					l.logger.Println(fmt.Sprintf("SUCCESS deleting %s!", d.Name()))
+				}
+			}(d)
+
 		}
+
+		wg.Wait()
 	}
 
 	return nil
