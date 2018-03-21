8e092df7d2069295e59d2d8b573caf99973ef634
Genevieve LEsperance <glesperance@pivotal.io>
2018-03-21 11:04:07 -0700

    Backfill instance group manager test.
    Signed-off-by: Connor Braa <connor.braa@gmail.com>

---
+++ gcp/compute/fakes/instance_group_managers_client.go
@@ -0,0 +1,42 @@
+package fakes
+
+import gcpcompute "google.golang.org/api/compute/v1"
+
+type InstanceGroupManagersClient struct {
+	ListInstanceGroupManagersCall struct {
+		CallCount int
+		Receives  struct {
+			Zone string
+		}
+		Returns struct {
+			Output *gcpcompute.InstanceGroupManagerList
+			Error  error
+		}
+	}
+
+	DeleteInstanceGroupManagerCall struct {
+		CallCount int
+		Receives  struct {
+			Zone                 string
+			InstanceGroupManager string
+		}
+		Returns struct {
+			Error error
+		}
+	}
+}
+
+func (n *InstanceGroupManagersClient) ListInstanceGroupManagers(zone string) (*gcpcompute.InstanceGroupManagerList, error) {
+	n.ListInstanceGroupManagersCall.CallCount++
+	n.ListInstanceGroupManagersCall.Receives.Zone = zone
+
+	return n.ListInstanceGroupManagersCall.Returns.Output, n.ListInstanceGroupManagersCall.Returns.Error
+}
+
+func (n *InstanceGroupManagersClient) DeleteInstanceGroupManager(zone, instanceGroupManager string) error {
+	n.DeleteInstanceGroupManagerCall.CallCount++
+	n.DeleteInstanceGroupManagerCall.Receives.Zone = zone
+	n.DeleteInstanceGroupManagerCall.Receives.InstanceGroupManager = instanceGroupManager
+
+	return n.DeleteInstanceGroupManagerCall.Returns.Error
+}
