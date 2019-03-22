aece12de8d99817bee13d35480d1c9a1c782a259
Zach Gershman <zgershman@pivotal.io>
2019-03-22 14:56:03 -0700

    Delete attached volumes
    - We delete compute instances (aka servers) first then volumes so that they're orphaned
    - We wait until the volume is in a deletable status before deleting
    
    Signed-off-by: Nick Rohn <nrohn@pivotal.io>

--- openstack/volume_api.go
+++ openstack/volume_api.go
@@ -1,6 +1,9 @@
 package openstack
 
 import (
+	"errors"
+	"time"
+
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack/blockstorage/v3/volumes"
 	"github.com/gophercloud/gophercloud/pagination"
@@ -8,6 +11,8 @@ import (
 
 type VolumesAPI struct {
 	serviceClient *gophercloud.ServiceClient
+	waitTime      time.Duration
+	maxRetries    int
 }
 
 func (api VolumesAPI) GetVolumesPager() pagination.Pager {
@@ -23,5 +28,35 @@ func (api VolumesAPI) PageToVolumes(page pagination.Page) ([]volumes.Volume, err
 }
 
 func (api VolumesAPI) DeleteVolume(id string) error {
+	var retries int
+
+	for {
+		volume, err := getVolume(api.serviceClient, id)
+		if err != nil {
+			return err
+		}
+
+		status := volume.Status
+		if status == "available" || status == "error" || status == "error_restoring" || status == "error_extending" || status == "error_managing" {
+			break
+		}
+
+		if retries == api.maxRetries {
+			return errors.New("volume failed to reach desired state")
+		}
+
+		retries++
+		time.Sleep(api.waitTime)
+	}
+
 	return volumes.Delete(api.serviceClient, id, volumes.DeleteOpts{}).ExtractErr()
+}
+
+func getVolume(serviceClient *gophercloud.ServiceClient, id string) (*volumes.Volume, error) {
+	volume, err := volumes.Get(serviceClient, id).Extract()
+	if err != nil {
+		return &volumes.Volume{}, err
+	}
+
+	return volume, nil
 }
