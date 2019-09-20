d54f6dc958aed7eca9ec5960d3465678e663b387
Genevieve Lesperance <glesperance@pivotal.io>
2019-09-20 18:13:27 -0400

    Extract flag handling and checking for other env vars.
    - Backfill tests for other env vars.

---
+++ app/env_vars.go
@@ -0,0 +1,110 @@
+package app
+
+import "os"
+
+type Options struct {
+	Version bool `short:"v"  long:"version"                     description:"Print version."`
+
+	IAAS      string `short:"i"  long:"iaas"        env:"BBL_IAAS"  description:"The IaaS for clean up."  `
+	NoConfirm bool   `short:"n"  long:"no-confirm"                  description:"Destroy resources without prompting. This is dangerous, make good choices!"`
+	DryRun    bool   `short:"d"  long:"dry-run"                     description:"List all resources without deleting any."`
+	Filter    string `short:"f"  long:"filter"                      description:"Filtering resources by an environment name."`
+	Type      string `short:"t"  long:"type"                        description:"Type of resource to delete."`
+
+	AWSAccessKeyID       string `long:"aws-access-key-id"        env:"BBL_AWS_ACCESS_KEY_ID"        description:"AWS access key id."`
+	AWSSecretAccessKey   string `long:"aws-secret-access-key"    env:"BBL_AWS_SECRET_ACCESS_KEY"    description:"AWS secret access key."`
+	AWSSessionToken      string `long:"aws-session-token"        env:"BBL_AWS_SESSION_TOKEN"        description:"AWS session token."`
+	AWSRegion            string `long:"aws-region"               env:"BBL_AWS_REGION"               description:"AWS region."`
+	AzureClientID        string `long:"azure-client-id"          env:"BBL_AZURE_CLIENT_ID"          description:"Azure client id."`
+	AzureClientSecret    string `long:"azure-client-secret"      env:"BBL_AZURE_CLIENT_SECRET"      description:"Azure client secret."`
+	AzureTenantID        string `long:"azure-tenant-id"          env:"BBL_AZURE_TENANT_ID"          description:"Azure tenant id."`
+	AzureSubscriptionID  string `long:"azure-subscription-id"    env:"BBL_AZURE_SUBSCRIPTION_ID"    description:"Azure subscription id."`
+	GCPServiceAccountKey string `long:"gcp-service-account-key"  env:"BBL_GCP_SERVICE_ACCOUNT_KEY"  description:"GCP service account key path."`
+	VSphereIP            string `long:"vsphere-vcenter-ip"       env:"BBL_VSPHERE_VCENTER_IP"       description:"vSphere vCenter IP address."`
+	VSpherePassword      string `long:"vsphere-vcenter-password" env:"BBL_VSPHERE_VCENTER_PASSWORD" description:"vSphere vCenter password."`
+	VSphereUser          string `long:"vsphere-vcenter-user"     env:"BBL_VSPHERE_VCENTER_USER"     description:"vSphere vCenter username."`
+	VSphereDC            string `long:"vsphere-vcenter-dc"       env:"BBL_VSPHERE_VCENTER_DC"       description:"vSphere vCenter datacenter."`
+	NSXTManagerHost      string `long:"nsxt-manager-host"        env:"BBL_NSXT_MANAGER_HOST"        description:"NSX-T manager IP address or domain name."`
+	NSXTUser             string `long:"nsxt-username"            env:"BBL_NSXT_USERNAME"            description:"NSX-T manager username."`
+	NSXTPassword         string `long:"nsxt-password"            env:"BBL_NSXT_PASSWORD"            description:"NSX-T manager password."`
+	OpenstackAuthUrl     string `long:"openstack-auth-url"       env:"BBL_OPENSTACK_AUTH_URL"       description:"Openstack auth URL."`
+	OpenstackUsername    string `long:"openstack-username"       env:"BBL_OPENSTACK_USERNAME"       description:"Openstack username."`
+	OpenstackPassword    string `long:"openstack-password"       env:"BBL_OPENSTACK_PASSWORD"       description:"Openstack password."`
+	OpenstackDomain      string `long:"openstack-domain-name"    env:"BBL_OPENSTACK_DOMAIN"         description:"Openstack domain name."`
+	OpenstackTenant      string `long:"openstack-project-name"   env:"BBL_OPENSTACK_PROJECT"        description:"Openstack project name."`
+	OpenstackRegion      string `long:"openstack-region-name"    env:"BBL_OPENSTACK_REGION"         description:"Openstack region name."`
+}
+
+const (
+	AWS       = "aws"
+	GCP       = "gcp"
+	Azure     = "azure"
+	VSphere   = "vsphere"
+	NSXT      = "nsxt"
+	Openstack = "openstack"
+)
+
+type OtherEnvVars struct {
+}
+
+func NewOtherEnvVars() OtherEnvVars {
+	return OtherEnvVars{}
+}
+
+func (e OtherEnvVars) LoadConfig(o *Options) {
+	switch o.IAAS {
+	case AWS:
+		if o.AWSAccessKeyID == "" {
+			o.AWSAccessKeyID = os.Getenv("AWS_ACCESS_KEY_ID")
+		}
+		if o.AWSSecretAccessKey == "" {
+			o.AWSSecretAccessKey = os.Getenv("AWS_SECRET_ACCESS_KEY")
+		}
+		if o.AWSSessionToken == "" {
+			o.AWSSessionToken = os.Getenv("AWS_SESSION_TOKEN")
+		}
+		if o.AWSRegion == "" {
+			o.AWSRegion = os.Getenv("AWS_DEFAULT_REGION")
+		}
+	case Azure:
+		if o.AzureClientID == "" {
+			o.AzureClientID = os.Getenv("ARM_CLIENT_ID")
+		}
+		if o.AzureClientSecret == "" {
+			o.AzureClientSecret = os.Getenv("ARM_CLIENT_SECRET")
+		}
+		if o.AzureSubscriptionID == "" {
+			o.AzureSubscriptionID = os.Getenv("ARM_SUBSCRIPTION_ID")
+		}
+		if o.AzureTenantID == "" {
+			o.AzureTenantID = os.Getenv("ARM_TENANT_ID")
+		}
+	case GCP:
+		if o.GCPServiceAccountKey == "" {
+			o.GCPServiceAccountKey = os.Getenv("GOOGLE_CREDENTIALS")
+		}
+	case NSXT:
+		if o.NSXTManagerHost == "" {
+			o.NSXTManagerHost = os.Getenv("NSXT_MANAGER_HOST")
+		}
+		if o.NSXTUser == "" {
+			o.NSXTUser = os.Getenv("NSXT_USERNAME")
+		}
+		if o.NSXTPassword == "" {
+			o.NSXTPassword = os.Getenv("NSXT_PASSWORD")
+		}
+	case VSphere:
+		if o.VSphereUser == "" {
+			o.VSphereUser = os.Getenv("VSPHERE_USER")
+		}
+		if o.VSpherePassword == "" {
+			o.VSpherePassword = os.Getenv("VSPHERE_PASSWORD")
+		}
+		if o.VSphereDC == "" {
+			o.VSphereDC = os.Getenv("VSPHERE_DATACENTER")
+		}
+		if o.VSphereIP == "" {
+			o.VSphereIP = os.Getenv("VSPHERE_IP")
+		}
+	}
+}
---
+++ app/env_vars_test.go
@@ -0,0 +1,140 @@
+package app_test
+
+import (
+	"os"
+
+	"github.com/genevieve/leftovers/app"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("OtherEnvVars", func() {
+	var (
+		options      *app.Options
+		otherEnvVars app.OtherEnvVars
+	)
+	BeforeEach(func() {
+		options = &app.Options{}
+		otherEnvVars = app.NewOtherEnvVars()
+	})
+
+	Describe("LoadConfig", func() {
+		Context("AWS", func() {
+			BeforeEach(func() {
+				options.IAAS = "aws"
+				os.Setenv("AWS_ACCESS_KEY_ID", "banana")
+				os.Setenv("AWS_SECRET_ACCESS_KEY", "kiwi")
+				os.Setenv("AWS_SESSION_TOKEN", "pineapple")
+				os.Setenv("AWS_DEFAULT_REGION", "plum")
+			})
+
+			AfterEach(func() {
+				os.Unsetenv("AWS_ACCESS_KEY_ID")
+				os.Unsetenv("AWS_SECRET_ACCESS_KEY")
+				os.Unsetenv("AWS_SESSION_TOKEN")
+				os.Unsetenv("AWS_DEFAULT_REGION")
+			})
+
+			It("checks for other possible env vars", func() {
+				otherEnvVars.LoadConfig(options)
+
+				Expect(options.AWSAccessKeyID).To(Equal("banana"))
+				Expect(options.AWSSecretAccessKey).To(Equal("kiwi"))
+				Expect(options.AWSSessionToken).To(Equal("pineapple"))
+				Expect(options.AWSRegion).To(Equal("plum"))
+			})
+		})
+
+		Context("Azure", func() {
+			BeforeEach(func() {
+				options.IAAS = "azure"
+				os.Setenv("ARM_CLIENT_ID", "banana")
+				os.Setenv("ARM_CLIENT_SECRET", "kiwi")
+				os.Setenv("ARM_SUBSCRIPTION_ID", "pineapple")
+				os.Setenv("ARM_TENANT_ID", "plum")
+			})
+
+			AfterEach(func() {
+				os.Unsetenv("ARM_CLIENT_ID")
+				os.Unsetenv("ARM_CLIENT_SECRET")
+				os.Unsetenv("ARM_SUBSCRIPTION_ID")
+				os.Unsetenv("ARM_TENANT_ID")
+			})
+
+			It("checks for other possible env vars", func() {
+				otherEnvVars.LoadConfig(options)
+
+				Expect(options.AzureClientID).To(Equal("banana"))
+				Expect(options.AzureClientSecret).To(Equal("kiwi"))
+				Expect(options.AzureSubscriptionID).To(Equal("pineapple"))
+				Expect(options.AzureTenantID).To(Equal("plum"))
+			})
+		})
+
+		Context("GCP", func() {
+			BeforeEach(func() {
+				options.IAAS = "gcp"
+				os.Setenv("GOOGLE_CREDENTIALS", "banana")
+			})
+
+			AfterEach(func() {
+				os.Unsetenv("GOOGLE_CREDENTIALS")
+			})
+
+			It("checks for other possible env vars", func() {
+				otherEnvVars.LoadConfig(options)
+
+				Expect(options.GCPServiceAccountKey).To(Equal("banana"))
+			})
+		})
+
+		Context("NSXT", func() {
+			BeforeEach(func() {
+				options.IAAS = "nsxt"
+				os.Setenv("NSXT_MANAGER_HOST", "banana")
+				os.Setenv("NSXT_USERNAME", "kiwi")
+				os.Setenv("NSXT_PASSWORD", "pineapple")
+			})
+
+			AfterEach(func() {
+				os.Unsetenv("NSXT_MANAGER_HOST")
+				os.Unsetenv("NSXT_USERNAME")
+				os.Unsetenv("NSXT_PASSWORD")
+			})
+
+			It("checks for other possible env vars", func() {
+				otherEnvVars.LoadConfig(options)
+
+				Expect(options.NSXTManagerHost).To(Equal("banana"))
+				Expect(options.NSXTUser).To(Equal("kiwi"))
+				Expect(options.NSXTPassword).To(Equal("pineapple"))
+			})
+		})
+
+		Context("vSphere", func() {
+			BeforeEach(func() {
+				options.IAAS = "vsphere"
+				os.Setenv("VSPHERE_USER", "banana")
+				os.Setenv("VSPHERE_PASSWORD", "kiwi")
+				os.Setenv("VSPHERE_DATACENTER", "pineapple")
+				os.Setenv("VSPHERE_IP", "pineapple")
+			})
+
+			AfterEach(func() {
+				os.Unsetenv("VSPHERE_USER")
+				os.Unsetenv("VSPHERE_PASSWORD")
+				os.Unsetenv("VSPHERE_DATACENTER")
+				os.Unsetenv("VSPHERE_IP")
+			})
+
+			It("checks for other possible env vars", func() {
+				otherEnvVars.LoadConfig(options)
+
+				Expect(options.VSphereUser).To(Equal("banana"))
+				Expect(options.VSpherePassword).To(Equal("kiwi"))
+				Expect(options.VSphereDC).To(Equal("pineapple"))
+				Expect(options.VSphereIP).To(Equal("pineapple"))
+			})
+		})
+	})
+})
